5e3d3ae3f11879d06a66b6601030d38c7b19134a
petermendoza <peterjohnm2002@gmail.com>
2023-10-08 21:44:59 -0500

    update

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -35,7 +35,12 @@ const createContact = asyncHandler(async(req,res) => {
 //@access public
 
 const getContact = asyncHandler(async(req,res) => {
-    res.status(200).json({message: `Get contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@desc Update contact
@@ -43,7 +48,17 @@ const getContact = asyncHandler(async(req,res) => {
 //@access public
 
 const updateContact = asyncHandler(async(req,res) => {
-    res.status(200).json({message: `Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true}
+    );
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
