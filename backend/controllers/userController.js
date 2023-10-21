7e53a30591ac7f6ef8f46c505e0c0f593616ff84
petermendoza <peterjohnm2002@gmail.com>
2023-10-21 01:12:40 -0500

    added jwt functionality in usercontroller, does comparisons with database to see if login is valid, makes access token... also made new environment variable called ACCESS_TOKEN_SECRET which is the key for decrypting, and can be accessed with process.env.ACCESS_TOKEN_SECRET as well as other env variables

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
@@ -41,6 +42,29 @@ const registerUser = asyncHandler(async (req,res) => {
 //@access public
 
 const loginUser = asyncHandler(async (req,res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user =  await User.findOne({email});
+    //Compare password with hashed password
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: "1m" }
+        );
+        res.status(200).json({accessToken});
+    } else{
+        res.status(401);
+        throw new Error("Email or Password is not valid");
+    }
     res.json({message: "Login the user" });
 });
 
