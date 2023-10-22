6e0cddc863445eced30105a30e52adf067ea63a6
petermendoza <peterjohnm2002@gmail.com>
2023-10-22 03:37:45 -0500

    worked on token validation middleware, made new file and changed userROutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { registerUser, loginUser, currentUser } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 
 router.post("/register", registerUser)
 
 router.post("/login", loginUser)
 
-router.post("/current", currentUser)
+router.get("/current", validateToken, currentUser)
 
 module.exports = router;
